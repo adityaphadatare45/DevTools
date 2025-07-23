935819ada15edb05057951595b0981fee0f870f3
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-07-23 17:21:42 +0530

    welcome screen commit

--- lib/UI/welcome_screen.dart
+++ lib/UI/welcome_screen.dart
@@ -4,5 +4,15 @@ class WelcomeScreen extends StatefulWidget{
   const WelcomeScreen({super.key});
 
   @override
-  State<StatefulWidget> createState()=> 
+  State<StatefulWidget> createState() => _WelcomescreenState();
 }
+
+ class _WelcomescreenState extends State<WelcomeScreen>{
+ 
+ @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+ }
